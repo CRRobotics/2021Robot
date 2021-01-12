8ca9383965b48d8eed1cdd01cf56d3f8d1276731
lilrichie <59586372+lilrichie@users.noreply.github.com>
2021-01-12 18:16:21 -0500

    Added AutoIndexer
    Might not work

--- src/main/java/org/team639/robot/Commands/Indexer/AutoIndexer.java
+++ src/main/java/org/team639/robot/Commands/Indexer/AutoIndexer.java
@@ -1,6 +1,29 @@
 package org.team639.robot.Commands.Indexer;
 
-public class AutoIndexer
+public class AutoIndexer extends CommandBase
 {
+    private Index indexer;
+    private long startMillis;
+    private long waitTimeMillis;
+    private boolean done;
+    
+    public AutoIndexer()
+    {
+        indexer = Robot.getIndexer();
+        addRequirements(indexer);
+    }
 
+    public void execute()
+    {
+        if (indexer.getSensorValue())
+        {
+            indexer.turnOn();
+        }
+        else if (!indexer.getSensorValue())
+        {
+            indexer.turnOff();
+        }
+    }
 }
+
+//TODO might not work
